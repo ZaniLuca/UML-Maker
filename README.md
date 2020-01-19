947bc899a7dfe1d406256684ee64d78835221aee
Luca <59318963+ZaniLuca@users.noreply.github.com>
2020-01-19 19:08:20 +0100

    Version 1.0 Relase
    First relase of the program

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# UML-Maker
+# UML Maker
 An UML Table maker for python, it make an uml table from a python file
+
+## How to install:
+1. Download the zip file
+2. Install the packages in the requirements.txt:
+2.1  Open a terminal in the project folder than type:
+	```
+	pip install -r requirements.txt
+	```
+3. Launch UML_Maker.py
+
+## How to use:
+To make an uml table simply use the "open file" button, then import the file you want to make taple out of, press the run button (the one with the play image) and the table/s will get saved into the /UML_Maker/output as '*ClassName*.png'
+
+# Screenshots
+
+![Main](https://user-images.githubusercontent.com/59318963/72685891-bf0ab980-3aee-11ea-94c8-b1b345daf0ad.PNG)
+
+These are the Tables generated
+![Game](https://user-images.githubusercontent.com/59318963/72685893-c29e4080-3aee-11ea-95b5-458dffb1b60a.png)
+![Square](https://user-images.githubusercontent.com/59318963/72685894-c4680400-3aee-11ea-988f-d64651ba7bc2.png)
--- UML_Maker.py
+++ UML_Maker.py
@@ -1,5 +1,7 @@
 import os
+import platform
 import tkinter as tk
+import tkinter.messagebox
 import tkinter.scrolledtext as tkst
 from tkinter import filedialog
 
@@ -15,15 +17,19 @@ class Program:
         self.running = False
         self.hasFile = False
         self.hasClass = False
-        self.cwd = os.getcwd()
+        self.system = platform.system()
+        self.stw = 55 if self.system == 'Darwin' else 48
+        self.sth = 20 if self.system == 'Darwin' else 16
         self.lines = []
         self.classes = []
         self.attributes = []
         self.methods = []
-        # TODO better imports
-        self.font = ImageFont.truetype(r'C:/Windows/Fonts/Arial.ttf', 20)
-        self.bg = tk.PhotoImage(file='GUI.png')
-        self.play_img = tk.PhotoImage(file='play_btn.png')
+
+        self.assets_folder = os.path.join(os.path.dirname(__file__), 'assets')
+        self.font = ImageFont.truetype(os.path.join(self.assets_folder, 'Arial.ttf'), 20)
+        self.bg = tk.PhotoImage(file=os.path.join(self.assets_folder, 'GUI.png'))
+        self.play_img = tk.PhotoImage(file=os.path.join(self.assets_folder, 'play_btn.png'))
+        self.logo = os.path.join(self.assets_folder, 'logo.ico')
 
     def run(self):
         """
@@ -31,29 +37,22 @@ class Program:
         :return: None
         """
         if self.hasFile:
-            num_params = []
+            num_attributes = []
             num_methods = []
 
-            class_lines_index, n_class = self.extract_classes()
-            # TODO add error message when a class isn't found
-            init_lines_index, init_ends = self.search_init()
+            class_lines_index, num_class = self.extract_classes()
+            if self.hasClass:
+                init_lines_index, init_ends = self.search_init()
 
-            for i in range(n_class):
-                num_params.append(self.extract_attributes(i, init_lines_index, init_ends))
-                num_methods.append(self.extract_methods(i, class_lines_index))
-            self.recap(n_class, num_params, num_methods)
-            self.draw(n_class, num_params, num_methods)
+                for i in range(num_class):
+                    num_attributes.append(self.extract_attributes(i, init_lines_index, init_ends))
+                    num_methods.append(self.extract_methods(i, class_lines_index))
+                self.recap(num_class, num_attributes, num_methods)
+                self.draw(num_class, num_attributes, num_methods)
+            else:
+                alert = tkinter.messagebox.showwarning('Error!', 'No class has been found')
         else:
-            # TODO add error msg when there isn't a file
-            print('--No FILE detected--')
-
-    def close(self):
-        """
-        end the loop and closes the window
-        :return: None
-        """
-        self.running = False
-        self.root.destroy()
+            alert = tkinter.messagebox.showwarning('Error!', 'File not detected')
 
     def initialize(self):
         """
@@ -62,15 +61,19 @@ class Program:
         """
         self.root.geometry(str(self.width) + 'x' + str(self.height))
         self.root.title('UML Maker')
+        self.root.wm_iconbitmap(self.logo)
         self.root.resizable(False, False)
 
         panel = tk.Label(self.root, image=self.bg)
         panel.pack(side="bottom", fill="both", expand="yes")
 
-        text_field = tkst.ScrolledText(self.root, wrap=tk.WORD, width=48, height=16).place(x=46, y=210)
+        text_field = tkst.ScrolledText(self.root, wrap=tk.WORD, width=self.stw, height=self.sth)
+        text_field.place(x=46, y=210)
 
-        browse_btn = tk.Button(self.root, text='Open file', command=lambda: self.browse_file()).place(x=160, y=175)
-        make_btn = tk.Button(self.root, image=self.play_img, command=lambda: self.run()).place(x=400, y=170)
+        browse_btn = tk.Button(self.root, text='Open file', command=lambda: self.browse_file())
+        browse_btn.place(x=160, y=175)
+        make_btn = tk.Button(self.root, image=self.play_img, command=lambda: self.run())
+        make_btn.place(x=400, y=170)
 
         self.root.mainloop()
 
@@ -79,7 +82,7 @@ class Program:
         Command activated when button pressed
         :return: None
         """
-        self.root.filename = filedialog.askopenfilename(initialdir=self.cwd, title='Pick a python file',
+        self.root.filename = filedialog.askopenfilename(initialdir=os.getcwd(), title='Pick a python file',
                                                         filetypes=(('Python Files', '*.py'), ("all files", "*.*")))
 
         if self.root.filename:
@@ -104,7 +107,7 @@ class Program:
         Updates the ScrolledText with the file content
         :return: None
         """
-        text_field = tkst.ScrolledText(self.root, wrap=tk.WORD, width=48, height=16)
+        text_field = tkst.ScrolledText(self.root, wrap=tk.WORD, width=self.stw, height=self.sth)
 
         for line in range(len(self.lines)):
             text_field.insert('insert', self.lines[line])
@@ -123,6 +126,8 @@ class Program:
         self.classes = []
         self.attributes = []
         self.methods = []
+        self.hasFile = False
+        self.hasClass = False
         self.update_text_field()
 
     def extract_classes(self):
@@ -131,17 +136,18 @@ class Program:
         :returns: int
         """
         class_lines_index = []
-        n_class = 0
+        num_class = 0
 
         for line in range(len(self.lines)):
             if "class " in self.lines[line]:
                 if "#" in self.lines[line]:
                     pass
                 elif ":" in self.lines[line]:
+                    self.hasClass = True
                     class_lines_index.append(line)
-                    n_class += 1
+                    num_class += 1
 
-        for classe in range(n_class):
+        for classe in range(num_class):
             class_line = self.lines[class_lines_index[classe]]
             class_name = ""
             for char in range(6, len(class_line)):
@@ -150,7 +156,7 @@ class Program:
                 class_name += class_line[char]
             self.classes.append(class_name)
 
-        return class_lines_index, n_class
+        return class_lines_index, num_class
 
     def search_init(self):
         """
@@ -162,7 +168,6 @@ class Program:
         for line in range(len(self.lines)):
             if "__init__" in self.lines[line]:
                 init_lines_index.append(line)
-                # CERCHIAMO LA FINE DELL'INIT
                 for init_lines in range(line + 1, len(self.lines)):
                     if "def " in self.lines[init_lines]:
                         init_ends.append(init_lines)
@@ -173,55 +178,53 @@ class Program:
     def extract_attributes(self, i, init_lines_index, init_ends):
         """
         extract attributes from the file
-        :param i: int
-        :param init_lines_index: int
-        :param init_ends: int
+        :attribute i: int
+        :attribute init_lines_index: int
+        :attribute init_ends: int
         :return: int
         """
         got_comment = False
         comment_lines = 0
 
-        # CERCO COMMENTI
         for line in range(init_lines_index[i] + 1, init_ends[i]):
             if got_comment and '"""' in self.lines[line]:
-                end_comment = line + 1  # RIGA DOVE FINISCONO
+                end_comment = line + 1
             if not got_comment and '"""' in self.lines[line]:
                 got_comment = True
-                start_comment = line  # RIGA DOVE INZIANO I COMMENTI
+                start_comment = line
 
-        # CALCOLO NUMERO DI COMMENTI
         if got_comment:
             comment_lines = end_comment - start_comment
 
-        params_start_pos = init_lines_index[i] + 1 + comment_lines
-        count_param_lines = 0
-        params_lines = []
+        attributes_start_pos = init_lines_index[i] + 1 + comment_lines
+        count_attributes_lines = 0
+        attributes_lines = []
 
-        for line in range(params_start_pos, len(self.lines)):
+        for line in range(attributes_start_pos, len(self.lines)):
             if "def " in self.lines[line]:
                 break
-            count_param_lines += 1
-            params_lines.append(self.lines[line])
+            count_attributes_lines += 1
+            attributes_lines.append(self.lines[line])
 
-        self_param_lines = []
+        self_attributes_lines = []
 
-        for line in range(len(params_lines)):
-            if "self." and "=" in params_lines[line]:
-                if "#" in params_lines[line]:
+        for line in range(len(attributes_lines)):
+            if "self." and "=" in attributes_lines[line]:
+                if "#" in attributes_lines[line]:
                     pass
                 else:
-                    self_param_lines.append(params_lines[line])
+                    self_attributes_lines.append(attributes_lines[line])
 
-        for line in range(len(self_param_lines)):
-            param = ""
-            element = str(self_param_lines[line].split())
+        for line in range(len(self_attributes_lines)):
+            attribute = ""
+            element = str(self_attributes_lines[line].split())
             for char in range(7, len(element)):
                 if element[char] == " " or element[char] == "." or element[char] == "(" or element[char] == "'":
                     break
-                param += element[char]
-            self.attributes.append(param)
+                attribute += element[char]
+            self.attributes.append(attribute)
 
-        return len(self_param_lines)
+        return len(self_attributes_lines)
 
     def extract_methods(self, i, class_lines_index):
         """
@@ -230,7 +233,6 @@ class Program:
         :param class_lines_index: int
         :return: int
         """
-        # TODO better names
         conta_methods = 0
 
         methods_lines = []
@@ -252,82 +254,87 @@ class Program:
         for element in range(conta_methods):
             method = ""
             new_line = str(methods_lines[element].split())
-            for lettere in range(9, len(new_line)):
-                if new_line[lettere] == "(" or new_line[lettere] == "'":
+            for char in range(9, len(new_line)):
+                if new_line[char] == "(" or new_line[char] == "'":
                     break
-                method += new_line[lettere]
+                method += new_line[char]
             self.methods.append(method + '()')
 
         return conta_methods
 
-    def draw(self, n_class, num_params, num_methods):
+    def draw(self, num_class, num_attributes, num_methods):
         """
         Draw and open the tables
-        :param n_class: int
-        :param num_params: int
+        :param num_class: int
+        :param num_attributes: int
         :param num_methods: int
         :return: None
         """
         width = 300
         row = 35
-        # TODO add color variables
-        params_exam = 0
+        attributes_exam = 0
         methods_exam = 0
 
-        for i in range(n_class):
-            height = row * (num_params[i] + num_methods[i]) + row
+        for i in range(num_class):
+            # Colors
+            class_color = "#8c8c8c"
+            attributes_color = "#FFFFFF"
+            methods_color = "#d9d9d9"
 
+            height = row * (num_attributes[i] + num_methods[i]) + row
             img = Image.new("RGB", (width, height))
-            shape = [(0, 0), (width, row)]
 
+            shape = [(0, 0), (width, row)]
             rect = ImageDraw.Draw(img)
-            rect.rectangle(shape, fill="#8c8c8c")
+            rect.rectangle(shape, fill=class_color)
 
             text = str(self.classes[i])
             rect.text((width // 2 - 30, 5), text, font=self.font, align="center", fill='black')
 
-            for param in range(params_exam, params_exam + num_params[i]):
-                shape = [(0, 35 + row * (param - params_exam + 1)), (width, 35 + (row * (param - params_exam) + 1))]
+            for attribute in range(attributes_exam, attributes_exam + num_attributes[i]):
+                shape = [(0, 35 + row * (attribute - attributes_exam + 1)),
+                         (width, 35 + (row * (attribute - attributes_exam) + 1))]
                 rect = ImageDraw.Draw(img)
 
-                rect.rectangle(shape, fill="white")
+                rect.rectangle(shape, fill=attributes_color)
 
-                text = str(self.attributes[param])
-                rect.text((5, 40 + row * (param - params_exam)), text, font=self.font, align="center", fill='black')
+                text = str(self.attributes[attribute])
+                rect.text((5, 40 + row * (attribute - attributes_exam)), text, font=self.font, align="center",
+                          fill='black')
 
             for method in range(methods_exam, methods_exam + num_methods[i]):
-                shape = [(0, 0 + num_params[i] * row + row * (method - methods_exam + 1)),
-                         (width, 35 + num_params[i] * row + row * (method - methods_exam + 1))]
+                shape = [(0, 0 + num_attributes[i] * row + row * (method - methods_exam + 1)),
+                         (width, 35 + num_attributes[i] * row + row * (method - methods_exam + 1))]
                 rect = ImageDraw.Draw(img)
 
-                rect.rectangle(shape, fill="#d9d9d9")
+                rect.rectangle(shape, fill=methods_color)
 
                 text = str(self.methods[method])
-                rect.text((5, 40 + num_params[i] * row + row * (method - methods_exam)), text, font=self.font,
+                rect.text((5, 40 + num_attributes[i] * row + row * (method - methods_exam)), text, font=self.font,
                           align="center", fill='black')
 
-            params_exam += num_params[i]
+            attributes_exam += num_attributes[i]
             methods_exam += num_methods[i]
 
             img.show()
-            img.save('C:/Users/Public/Documents/UML/output/'+ self.classes[i]+'.png', format='png')
+            img.save(os.path.join(os.getcwd(), 'output') + '/' + self.classes[i] + '.png', format='png')
 
-    def recap(self, n_class, num_params, num_methods):
+    def recap(self, num_class, num_attributes, num_methods):
 
-        params_exam = 0
+        attributes_exam = 0
         methods_exam = 0
 
-        for i in range(n_class):
+        for i in range(num_class):
 
             frase = "class " + self.classes[i] + ": \nattributes: "
-            for param in range(params_exam, params_exam + num_params[i]):
-                frase += self.attributes[param] + ", "
+            for attribute in range(attributes_exam, attributes_exam + num_attributes[i]):
+                frase += self.attributes[attribute] + ", "
 
             frase += "\nmethods: "
             for method in range(methods_exam, methods_exam + num_methods[i]):
                 frase += self.methods[method] + ", "
 
-            params_exam += num_params[i]
+            attributes_exam += num_attributes[i]
             methods_exam += num_methods[i]
             print(frase)
 
